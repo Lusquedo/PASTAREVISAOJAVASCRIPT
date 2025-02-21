7d85cd488b1fbc5063cecfae0e3a3a464f304041
Lucas Ramos da Silva <Luscas.Ramos.da.Silva@gmail.com>
2025-02-20 22:36:26 -0300

    Add files via upload

---
+++ PASTAREVISAOJAVASCRIPT/main.js
@@ -0,0 +1,9 @@
+window.alert("asdf")
+
+let pAllanTuring = new Pessoa();
+let pVonNewmann = new Pessoa();
+pAllanTuring.nome = "Allan Turing";
+pVonNewmann.nome = "Von Newmann";
+
+console.log(pAllanTuring);
+console.log(pVonNewmann);
